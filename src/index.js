c57262b0f9618ddf989702939eea14c3bddf0d63
AnhTai2k4 <tranthanhanhtai2909@gmail.com>
2025-08-21 23:43:32 +0700

    remove css

---
+++ src/index.js
@@ -0,0 +1,32 @@
+const express = require('express') ;
+const hbs = require('express-handlebars');
+const morgan = require('morgan');
+const path = require('path')
+
+
+const app = express()
+const port = 3000
+
+app.use(express.static(path.join(__dirname,'public')))
+
+//http logger
+app.use(morgan('combined'))
+
+//template engine 
+app.engine('hbs', hbs.engine({
+  extname: '.hbs'
+}));
+app.set('view engine', 'hbs');
+app.set('views', path.join(__dirname,'resources/views'));
+
+app.get('/', (req, res) => {
+  res.render('home')
+})
+
+app.get('/news', (req, res) => {
+  res.render('news')
+})
+//127.0.0.1 - localhost
+app.listen(port, () => {
+  console.log(`Example app listening on port http://localhost:${port}`)
+})
