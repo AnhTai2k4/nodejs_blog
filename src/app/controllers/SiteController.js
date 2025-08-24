f534edc3269d13d38b5d78377d0b0fbb2cdc178c
AnhTai2k4 <tranthanhanhtai2909@gmail.com>
2025-08-25 01:28:50 +0700

    add config + model Course

--- src/app/controllers/SiteController.js
+++ src/app/controllers/SiteController.js
@@ -1,8 +1,20 @@
+const Course = require('../models/Course')
+
 class SiteController {
 
-    //[Get] /
-    index(req,res){
-    res.render('home')
+     //[Get] /
+    async index(req, res) {
+        Course.find({})
+        .then(courses => {
+            // Nếu không có lỗi, trả về danh sách khóa học dưới dạng JSON
+            res.json(courses);
+        })
+        .catch(err => {
+            // Nếu có lỗi, xử lý lỗi và trả về một trạng thái lỗi hoặc thông báo
+            console.error(err);
+            res.status(500).json({ error: 'Internal Server Error' });
+        });
+        // res.render('home')
     }
 
     //[Get] /search
