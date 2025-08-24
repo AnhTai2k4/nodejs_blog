ad1d2ea8418ae1f80b937932e924dc22566e2ff2
AnhTai2k4 <tranthanhanhtai2909@gmail.com>
2025-08-24 13:41:40 +0700

    Add router+ controler

--- src/index.js
+++ src/index.js
@@ -2,10 +2,10 @@ const express = require('express') ;
 const hbs = require('express-handlebars');
 const morgan = require('morgan');
 const path = require('path')
-
-
 const app = express()
 const port = 3000
+
+const route = require('./routes/index.js')
 
 app.use(express.static(path.join(__dirname,'public')))
 
@@ -19,13 +19,12 @@ app.engine('hbs', hbs.engine({
 app.set('view engine', 'hbs');
 app.set('views', path.join(__dirname,'resources/views'));
 
-app.get('/', (req, res) => {
-  res.render('home')
-})
+//Home, search, contact
 
-app.get('/news', (req, res) => {
-  res.render('news')
-})
+//Routes init
+route(app)
+
+
 //127.0.0.1 - localhost
 app.listen(port, () => {
   console.log(`Example app listening on port http://localhost:${port}`)
